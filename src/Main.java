24726b39abc5bf6d9fcd94e98c3ce37565292be9
parksil <parksil00@naver.com>
2020-11-07 12:22:08 +0900

    배열

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        sc.nextLine();
         
-		Scanner sc = new Scanner(System.in);
-		List<Integer> nums = new ArrayList<>();
-		
-		for(int i = 0; i < 10; i++) {
-			int num = sc.nextInt();
-			num %= 42;
-			if(!nums.contains(num)) {
-				nums.add(num);
-			}
-				
-		}
-		
-		System.out.println(nums.size());
-
+        for(int i = 0; i < length; i++) {
+        	String ox = sc.nextLine();
+        	String[] ox2 = ox.split("");
+        	
+        	int total = 0;	//통합점수
+        	for(int j = 0; j < ox2.length; j++) {
+        		
+        		int score = 0;	//연속점수
+        		
+        		if(ox2[j].equals("O")) {
+        			score += 1;
+        		} else if(ox2[j].equals("X")) {
+        			total += score;
+        			score = 0;
+        		}
+        		
+        		if(j == ox2.length-1) {
+        			total += score;
+        		}
+        	}
+        	System.out.println(total);
+        }
 	}
 }
