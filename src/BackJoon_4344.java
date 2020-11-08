8323a1ec3f0558718767eaab7957004c7657edcb
parksil <parksil00@naver.com>
2020-11-08 22:59:26 +0900

    배열

--- src/BackJoon_4344.java
+++ src/BackJoon_4344.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BackJoon_4344 {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
