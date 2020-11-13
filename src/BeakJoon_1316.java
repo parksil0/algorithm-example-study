553a35cb690e18fb8b0cbfece523ae967c94e366
parksil <parksil00@naver.com>
2020-11-13 18:06:31 +0900

    문자열

--- src/BeakJoon_1316.java
+++ src/BeakJoon_1316.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BeakJoon_1316 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -42,4 +42,3 @@ public class Main {
 		
 	}
 }
-
