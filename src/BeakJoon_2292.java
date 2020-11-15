2870f9e9265561e960904bb69dea6eacaf0e0ae6
parksil <parksil00@naver.com>
2020-11-15 20:39:42 +0900

    수학 1

--- src/BeakJoon_2292.java
+++ src/BeakJoon_2292.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class BeakJoon_2292 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,4 +36,3 @@ public class Main {
 		
 	}
 }
-
