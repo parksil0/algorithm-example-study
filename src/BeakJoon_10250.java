9c9d62da5a16d8114fffed8ea7a701fa7d5e1287
parksil <parksil00@naver.com>
2020-11-18 00:03:35 +0900

    수학 1

--- src/BeakJoon_10250.java
+++ src/BeakJoon_10250.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class BeakJoon_10250 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int length = Integer.parseInt(br.readLine());
---
+++ src/BeakJoon_2869.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BeakJoon_2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		
+		int A = Integer.parseInt(input[0]);
+		int B = Integer.parseInt(input[1]);
+		int V = Integer.parseInt(input[2]);
+		
+		int num1 = V-B;
+		int num2 = A-B;
+		
+		if(num1 % num2 == 0) {
+			System.out.println(num1 / num2);
+		} else {
+			System.out.println((num1 / num2) + 1);
+			
+		}
+		
+	}
+	/*
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		
+		double A = Integer.parseInt(input[0]);
+		double B = Integer.parseInt(input[1]);
+		double V = Integer.parseInt(input[2]);
+		
+		double num1 = V-A;
+		double num2 = A-B;
+		
+		double result =  Math.ceil(num1 / num2);
+		
+		System.out.println((int)(result+1));
+	}
+	*/
+}
