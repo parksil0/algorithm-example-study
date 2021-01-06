b09d5b74795133bb1c5633ab7954b08f6cacfe5a
parksil <parksil00@naver.com>
2021-01-06 23:31:24 +0900

    백준 프로그래밍 1978번 [수학2] 소수 찾기 재풀이

--- src/BOJ_1978.java
+++ src/BOJ_1978.java
@@ -1,28 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ_1978 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int length = Integer.parseInt(br.readLine());
-		String[] inputArr = br.readLine().split(" ");
-		
-		int count = 0;
-		
-		for(int i = 0; i < length; i++) {
-			
-			if(Integer.parseInt(inputArr[i]) <= 1) {
-				count++;
+	public static void main(String[] args) {
+		int nonPrime = 0;
+
+		Scanner sc = new Scanner(System.in);
+		int length = Integer.parseInt(sc.nextLine());
+
+		String[] arr = sc.nextLine().split(" ");
+
+		for(int i = 0; i < arr.length; i++) {
+
+			if(Integer.parseInt(arr[i]) <= 1) {
+				nonPrime++;
 			} else {
-				for(int j = 2; j < Integer.parseInt(inputArr[i]); j++) {
-					if(Integer.parseInt(inputArr[i]) % j == 0) {
-						count++;
+
+				for (int j = 2; j < Integer.parseInt(arr[i]); j++) {
+					if (Integer.parseInt(arr[i]) % j == 0) {
+						nonPrime++;
 						break;
 					}
 				}
 			}
 		}
-		System.out.println(length - count);
+
+		System.out.println(length - nonPrime);
 	}
+
 }
