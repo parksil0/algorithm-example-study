4f6cf486955a90764d7b0169c016e496012584e5
parksil <parksil00@naver.com>
2021-01-19 00:18:23 +0900

    백준 프로그래밍 10870번 [재귀] 피보나치 수 5

---
+++ src/BOJ_10870.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int result = fibonacci(input);
+        System.out.println(result);
+    }
+
+    static int fibonacci(int num) {
+        if(num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        } else {
+            return fibonacci(num-2) + fibonacci(num-1);
+        }
+    }
+}
