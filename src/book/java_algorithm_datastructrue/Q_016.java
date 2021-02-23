3562ae0fea01e044ebfec33752961f4ab082043a
parksil <parksil00@naver.com>
2021-02-23 23:05:42 +0900

    별 피라미드

---
+++ src/book/java_algorithm_datastructrue/Q_016.java
@@ -0,0 +1,32 @@
+package book.java_algorithm_datastructrue;
+
+import java.util.*;
+import java.io.*;
+
+public class Q_016 {
+
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        sb = new StringBuilder();
+
+        spira(num);
+        System.out.println(sb.toString());
+    }
+
+    static void spira(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = n - (i+1); j > 0; j--) {
+                sb.append(" ");
+            }
+
+            for(int k = 0; k < (i+1)*2-1; k++) {
+                sb.append("*");
+            }
+            sb.append('\n');
+        }
+    }
+}
