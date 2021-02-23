cdfbc4de9fb0a4da09c219a83acd1123f48fa04a
parksil <parksil00@naver.com>
2021-02-23 23:05:33 +0900

    입력받은 수의 자리 수 구하기

---
+++ src/book/java_algorithm_datastructrue/Q_011.java
@@ -0,0 +1,13 @@
+package book.java_algorithm_datastructrue;
+
+import java.util.*;
+import java.io.*;
+
+public class Q_011 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        System.out.println(input + "은 " + input.length() + "자리 수 입니다.");
+    }
+}
