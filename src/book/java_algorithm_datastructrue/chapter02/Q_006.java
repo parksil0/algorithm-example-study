d1645e4dc428b2956949c5681978623fb005ee4d
parksil <parksil00@naver.com>
2021-02-25 19:40:45 +0900

    n진수 소수 출력

---
+++ src/book/java_algorithm_datastructrue/chapter02/Q_006.java
@@ -0,0 +1,51 @@
+package book.java_algorithm_datastructrue.chapter02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q_006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int no, cd, dno, retry;
+        char[] cno = new char[32];
+
+        System.out.println("10진수 기수 변환");
+        do {
+            do {
+                System.out.print("변환하는 음이 아닌 정수: ");
+                no = sc.nextInt();
+            } while(no < 0);
+
+            do {
+                System.out.print("어떤 진수로 변환할까요? (2~36) : ");
+                cd = sc.nextInt();
+            } while(cd < 2 || cd > 36);
+            System.out.println("before : " + Arrays.toString(cno));
+
+            dno = cardConvR(no, cd, cno);
+
+            System.out.println("dno : " + dno);
+            System.out.print(cd + "진수로는 ");
+            for(int i = dno - 1; i >= 0; i--) {
+                System.out.print(cno[i]);
+            }
+            System.out.println("입니다.");
+
+            System.out.println("after : " + Arrays.toString(cno));
+
+            System.out.print("한 번 더 할까요? (1. 예/ 0. 아니오) : ");
+            retry = sc.nextInt();
+        } while(retry == 1);
+    }
+
+    static int cardConvR(int x, int r, char[] d) {
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+        do {
+            d[digits++] = dchar.charAt(x % r);
+            x /= r;
+        } while( x != 0 );
+        return digits;
+    }
+}
