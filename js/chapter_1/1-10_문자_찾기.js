7fc205200d8c450925eedaf6fe2b362553ba54b3
parksil0 <parksil00@naver.com>
2021-07-04 20:54:45 +0900

    문자 찾기

---
+++ js/chapter_1/1-10_문자_찾기.js
@@ -0,0 +1,32 @@
+/*
+한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+문자열의 길이는 100을 넘지 않습니다.
+▣ 입력설명
+첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+▣ 출력설명
+첫 줄에 해당 문자의 개수를 출력한다.
+▣ 입력예제 1 
+  COMPUTERPROGRAMMING R
+▣ 출력예제 1 
+  3
+*/
+
+// function solution(str1, str2) {
+//   const arr = str1.split("");
+//   let count = 0;
+
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] === str2) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+function solution(str1, str2) {
+  return str1.split(str2).length -1;
+}
+
+let str="COMPUTERPROGRAMMING";
+console.log(solution(str, 'R'));
